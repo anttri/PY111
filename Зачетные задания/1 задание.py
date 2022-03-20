8dcc92630f675959504780c305f67a98337b52a6
Anton <ant.trifonenkov@yandex.ru>
2022-03-20 20:51:23 +0300

    answering about first task

---
+++ Зачетные задания/1 задание.py
@@ -0,0 +1,10 @@
+a = len(arr) - 1 #O(1)
+out = list() #O(1)
+while a > 0: #O(n)
+    out.append(arr[a]) #O(1)
+    a = a // 1.7 #O(1)
+out.merge_sort() #O(n*log(n))
+
+#ОТВЕТ: сложность алгоритма  O(n log n)
+
+
