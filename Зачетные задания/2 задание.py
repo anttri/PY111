0fd38336f37b10a2d1004c02a5f9820378704bde
Anton <ant.trifonenkov@yandex.ru>
2022-03-26 16:01:15 +0300

    2 задание, исправление

--- Зачетные задания/2 задание.py
+++ Зачетные задания/2 задание.py
@@ -1,35 +1,18 @@
 def rhyme(n, k):
-
-    beaten_person = 0
     new_list = []
     for i in range(0, n):
         i += 1
         new_list.append(i)
     print(new_list)
 
-    while True:
-        if len(new_list) == 1:
-            print(f'Победил: {new_list[0]}')
-            break
-        else:
-            if k > n:
-                del_person = k - n - 1 + beaten_person
-                if del_person >= len(new_list):
-                    del_person = del_person - len(new_list)
-            else:
-                del_person = k - 1 + beaten_person
-                while len(new_list) <= del_person:
-                    del_person = del_person - len(new_list)
+    while n > 1:
 
-            del new_list[del_person]
+        del_person = k % n
+        del new_list[del_person - 1]
+        n -= 1
 
-            beaten_person = del_person
 
-            if beaten_person > len(new_list):
-                beaten_person = beaten_person - len(new_list) - 1
-            print(new_list)
-
-    return None
+    print(f'Победил: {new_list[0]}')
 
 
 if __name__ == '__main__':
